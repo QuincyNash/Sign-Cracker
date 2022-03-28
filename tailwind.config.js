a6fb7c998f7fc13ebcb8dc8a97fd222c291920b4
quincynash <quincy.nash@icloud.com>
2022-03-27 21:22:39 -0700

    Remove Unused Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,6 @@ module.exports = {
     fontFamily: {
       'roboto': ['Roboto', 'sans-serif'],
     },
-    extend: {
-      colors: {
-        'cool-white': '#dcdcdc',
-        'bright-blue': '#6699cc',
-        'blue-submit': '#2196f3',
-      },
-      keyframes: {
-        fadein: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        }
-      },
-      animation: {
-        fadein: 'fadein 150ms',
-      },
-    },
   },
   plugins: [],
 }
